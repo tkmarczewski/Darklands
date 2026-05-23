b8d0e05aed03240f46f0c5e17dd0d1393669fafe
tkmarczewski <t.k.marczewski@gmail.com>
2026-05-23 12:31:33 +0200

    Create WorldState.kt

---
+++ app/src/main/java/com/darklandsmobile/core/WorldState.kt
@@ -0,0 +1,10 @@
+package com.darklandsmobile.core
+
+data class WorldState(
+    var region: String = "town",
+    var location: String = "Magdeburg",
+    var day: Int = 1,
+    var timeOfDay: String = "morning",
+    var fatigue: Int = 0,
+    var lastEncounter: String = "none"
+)
