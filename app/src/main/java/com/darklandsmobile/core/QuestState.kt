1ba8ac823a7a92cc5f5bc3f02fde6590f4d896a0
tkmarczewski <t.k.marczewski@gmail.com>
2026-05-23 12:32:28 +0200

    Create QuestState.kt

---
+++ app/src/main/java/com/darklandsmobile/core/QuestState.kt
@@ -0,0 +1,7 @@
+package com.darklandsmobile.core
+
+data class QuestState(
+    val activeQuests: MutableList<String> = mutableListOf(),
+    val completedQuests: MutableList<String> = mutableListOf(),
+    val questProgress: MutableMap<String, Int> = mutableMapOf()
+)
