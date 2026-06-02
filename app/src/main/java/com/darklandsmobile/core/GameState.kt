467e3e35a8f6cb809491efe3d0095379d5ff1e90
tkmarczewski <t.k.marczewski@gmail.com>
2026-06-02 15:13:30 +0200

    mutableListOfOf bugfix

--- app/src/main/java/com/darklandsmobile/core/GameState.kt
+++ app/src/main/java/com/darklandsmobile/core/GameState.kt
@@ -2,10 +2,10 @@ package com.darklandsmobile.core
 
 data class GameState(
     val world: WorldState = WorldState(),
-    val party: MutableList<Hero> = mutableListOfOf(),
-    val inventory: MutableList<Item> = mutableListOfOf(),
+    val party: MutableList<Hero> = mutableListOf(),
+    val inventory: MutableList<Item> = mutableListOf(),
     var activeHeroId: String? = null,
-    val logEntries: MutableList<String> = mutableListOfOf(),
+    val logEntries: MutableList<String> = mutableListOf(),
     val combat: CombatState = CombatState(),
     val prayer: PrayerState = PrayerState(),
     val reputation: ReputationState = ReputationState(),
