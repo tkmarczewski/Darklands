4a71425405943b45038fd8315d2dba176dea57ac
tkmarczewski <t.k.marczewski@gmail.com>
2026-06-14 23:58:27 +0200

    QuestState: add activeEndgameQuests and completedEndgameQuests for main quest tracking

--- app/src/main/java/com/grimreich/core/QuestState.kt
+++ app/src/main/java/com/grimreich/core/QuestState.kt
@@ -3,5 +3,8 @@ package com.grimreich.core
 data class QuestState(
     val activeQuests: MutableList<String> = mutableListOf(),
     val completedQuests: MutableList<String> = mutableListOf(),
-    val questProgress: MutableMap<String, Int> = mutableMapOf()
+    val questProgress: MutableMap<String, Int> = mutableMapOf(),
+    // Endgame (main) quest tracking
+    val activeEndgameQuests: MutableList<String> = mutableListOf(),
+    val completedEndgameQuests: MutableList<String> = mutableListOf()
 )
